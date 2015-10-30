1e01441867733caf113ef058ee0866ecd2b62188
alumno <alumno@2A5PC08>
2015-10-30 10:12:50 +0100

    security

--- src/java/com/fpmislata/banco/presentation/database/ServletContextListenerImpl.java
+++ src/java/com/fpmislata/banco/presentation/database/ServletContextListenerImpl.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.fpmislata.banco.presentation;
+package com.fpmislata.banco.presentation.database;
 
 import com.fpmislata.banco.business.domain.EntidadBancaria;
 import com.fpmislata.banco.presentation.json.JsonTransformer;
